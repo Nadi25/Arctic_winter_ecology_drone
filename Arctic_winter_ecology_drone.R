cfdbc1bb2310dd8adf154db3103ebd65d323e6c3
Nadi25 <nadine-arzt@gmx.de>
2026-02-04 16:03:14 +0100

    script

--- Arctic_winter_ecology_drone.R
+++ Arctic_winter_ecology_drone.R
@@ -6,17 +6,23 @@
 # Drone -------------------------------------------------------------------
 
 
+# install packages --------------------------------------------------------
+# install.packages(c("sf", "terra", "mapview", "RStoolbox", "ggplot2",
+#                    "amt", "spatstat", "adehabitatHR", "caret", "randomForest"))
 
 
 
-
-
-
-
-
-
-
-
+# load packages -----------------------------------------------------------
+library(sf)
+library(terra)
+library(mapview)
+library(RStoolbox)
+library(ggplot2)
+library(amt)
+library(spatstat)
+library(adehabitatHR)
+library(caret)
+library(randomForest)
 
 
 
